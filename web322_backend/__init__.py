114a02362d8523e4f9c34313b37aeb76d8c465a1
Hugo Berg <ilykxar@gmail.com>
2023-04-15 14:48:28 +0200

    something mergin

--- web322_backend/__init__.py
+++ web322_backend/__init__.py
@@ -4,17 +4,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+from flask_api_key import APIKeyManager
 
 db = SQLAlchemy()
 migrate = Migrate()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
+apikey_manager = APIKeyManager()
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    print(app.config["SQLALCHEMY_DATABASE_URI"], os.environ.get("SECRET_KEY"))
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
     login_manager.session_protection = "strong"
@@ -25,11 +28,14 @@ def create_app():
     app.register_blueprint(main_blueprint)
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
+    from .serviceworker import serviceworker as serviceworker_blueprint
+    app.register_blueprint(serviceworker_blueprint)
 
     login_manager.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
     bcrypt.init_app(app)
+    apikey_manager.init_app(app)
 
     return app
 
--- web322_backend/models.py
+++ web322_backend/models.py
@@ -1,5 +1,11 @@
-from . import db
+from . import db, login_manager
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
 class User(db.Model, UserMixin):
@@ -7,8 +13,14 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
     pwd = db.Column(db.String(300), nullable=False)
 
+    def __init__(self, username, pwd):
+        self.username = username
+        self.pwd = generate_password_hash(pwd)
+
+    def verify_password(self, pwd):
+        return check_password_hash(self.pwd, pwd)
+
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
+        return f"User('{self.username}')"
